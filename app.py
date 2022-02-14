bc473aa903cae27096d428697be654644677f5f0
Gyan Prakash Tiwari <golu04822@gmail.com>
2022-02-14 14:09:28 +0530

    added assingment on /assingment route

--- app.py
+++ app.py
@@ -63,5 +63,53 @@ def delete(sno):
     return redirect("/")
 
 
+# Flask assingment 1
+
+@app.route("/assingment")
+def hello_world_assingment():
+    smap = {
+        1:"st",
+        2:"nd",
+        3:"rd",
+        4:"th"
+    }
+    smonth = {
+        1 : "Jan",
+        2 : "Feb",
+        3 : "Mar",
+        4 : "April",
+        5 : "May",
+        6 : "Jun",
+        7 : "Jul",
+        8 : "Aug",
+        9 : "Sep",
+        10 : "Oct",
+        11 : "Nov",
+        12 : "Dec"
+    }
+    curr_date = datetime.now().date()
+    suffix = smap[1]
+    dd = curr_date.day
+    mm = curr_date.month
+    yy = curr_date.year
+
+    # Handles the suffix of date part
+    last_digit = dd%10
+    tmpdd = dd/10
+    second_lastdigit = tmpdd%10
+
+    if second_lastdigit == 1:
+        suffix = "th"
+    else:
+        if last_digit > 3:
+            last_digit = 4
+        suffix = smap[last_digit]
+
+    mm = smonth[mm]
+
+    return render_template("index_a1.html",dd=dd,suffix = suffix,mm=mm,yy=yy) 
+
+
+
 if __name__ == "__main__":
     app.run(debug=True)
